8163d981f2b4cc5f546f415f04f7fbddf2d27c8e
baji <baji.lamallem@gmail.com>
2025-05-24 17:31:16 +0200

    accueil-fini

--- assets/app.js
+++ assets/app.js
@@ -4,19 +4,12 @@ import './bootstrap.js';
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
+ * 
  */
-
-
-// Je prends le JS de Bootstrap
-import 'bootstrap';
-
-
-// Je prends le CSS de Bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
-//  c'est notre css
 import './styles/app.css';
 
+// Je prends le CSS de Bootstrap
+import './vendor/bootstrap/dist/css/bootstrap.min.css';
+// Je prends le JS de Bootstrap
+import 'bootstrap';
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
