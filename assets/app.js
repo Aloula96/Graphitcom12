419760873a5477f867174d347a7a81475e062262
marina63500 <mar.59@hotmail.fr>
2025-05-26 10:58:02 +0200

    fin apropos fin

--- assets/app.js
+++ assets/app.js
@@ -6,10 +6,14 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  * 
  */
-import './styles/app.css';
+
 
 // Je prends le CSS de Bootstrap
 import './vendor/bootstrap/dist/css/bootstrap.min.css';
 // Je prends le JS de Bootstrap
 import 'bootstrap';
+
+
+import './styles/app.css';
+
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
