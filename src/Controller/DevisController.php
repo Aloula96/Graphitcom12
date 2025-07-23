1f6b3a98a01936b304879bf0f73b298eaea88283
Aminelamallem <baji.lamallem@gmail.com>
2025-07-23 12:20:27 +0200

    final185

--- src/Controller/DevisController.php
+++ src/Controller/DevisController.php
@@ -4,15 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Devis;
 use App\Form\DevisType;
-use App\Entity\SiteVitrine;
-use App\Form\SiteVitrineTypeForm;
-use Symfony\Component\Mime\Email;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 final class DevisController extends AbstractController
 {
@@ -27,64 +25,26 @@ final class DevisController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Sauvegarde en base
             $entityManager->persist($devis);
             $entityManager->flush();
 
+            // Envoi de l'e-mail
             $email = (new Email())
                 ->from('amine@example.com')
                 ->to('baji.lamallem@gmail.com')
                 ->subject('test devis')
-                ->html(
-                    "Nom: " . $devis->getLastName() . "<br>" .
-                    "Prénom: " . $devis->getFirstName() . "<br>" .
-                    "Email: " . $devis->getEmail() . "<br>" .
-                    "Téléphone: " . $devis->getPhone()
-                );
-
+                ->html($devis->getFirstName() );
+            
             $mailer->send($email);
 
+            // Redirection après traitement
             return $this->redirectToRoute('app_devis');
         }
 
         return $this->render('devis/index.html.twig', [
             'form' => $form->createView(),
             'devis' => $devis,
-        ]);
-    }
-
-    #[Route('/siteVitrine', name: 'app_vitrine')]
-    public function vitrine(
-        Request $request,
-        EntityManagerInterface $entityManager,
-        MailerInterface $mailer
-    ): Response {
-        $vitrine = new SiteVitrine();
-        $form = $this->createForm(SiteVitrineTypeForm::class, $vitrine);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($vitrine);
-            $entityManager->flush();
-
-            $email = (new Email())
-                ->from('amine@example.com')
-                ->to('baji.lamallem@gmail.com')
-                ->subject('devis site vitrine')
-                ->html(
-                    "Nom: " . $vitrine->getLastName() . "<br>" .
-                    "Prénom: " . $vitrine->getFirstName() . "<br>" .
-                    "Email: " . $vitrine->getEmail() . "<br>" .
-                    "Téléphone: " . $vitrine->getPhone()
-                );
-
-            $mailer->send($email);
-
-            return $this->redirectToRoute('app_devis');
-        }
-
-        return $this->render('siteVitrine/vitrine.html.twig', [
-            'form' => $form->createView(),
-            'vitrine' => $vitrine,
         ]);
     }
 }
